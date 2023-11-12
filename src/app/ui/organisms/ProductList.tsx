cd299244f160075d2165c885ce4f36fb07633790
marudazk <marudazk@gmail.com>
2023-11-12 20:20:16 +0100

    data-testid moved in the DOM

--- src/app/ui/organisms/ProductList.tsx
+++ src/app/ui/organisms/ProductList.tsx
@@ -3,7 +3,10 @@ import { type ProductItemType } from "@/app/ui/types/types";
 
 export const ProductList = ({ products }: { products: ProductItemType[] }) => {
   return (
-    <ul className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <ul
+      data-testid="products-list"
+      className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+    >
       {products.map((product) => {
         return <ProductListItem key={product.id} product={product} />;
       })}
