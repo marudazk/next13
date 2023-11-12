d8be6db526d1eabf7296da4551b9052b7beb3464
marudazk <marudazk@gmail.com>
2023-11-12 19:47:15 +0100

    Test attribute added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ const products: ProductItemType[] = [
 export default function Home() {
   return (
     <section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
-      <ProductList products={products} />
+      <ProductList data-testid="products-list" products={products} />
     </section>
   );
 }
